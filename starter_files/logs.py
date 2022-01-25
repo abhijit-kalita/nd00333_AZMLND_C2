ddbf5a98ef5e92de562293189d5bcba9b377f50a
abhijit-kalita <abhijit-kalita@users.noreply.github.com>
2022-01-25 23:59:39 +0530

    updated logs.py code

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -5,10 +5,12 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "model-deploymet-test"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
+# enable application insight
+service.update(enable_app_insights=True)
 logs = service.get_logs()
 
 for line in logs.split('\n'):
